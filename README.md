f2256f881b7f5f6fba1ab197858994900431a7ed
Joshua Rombauer <joshua.rombauer@tum.de>
2017-01-03 13:18:17 +0100

    rewrites tg-irc-bridge to discord-irc-bridge

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
-# Irc <-> Telegram Bridge 
+# IRC <-> Discord Bridge 
 
-This programm sends messages between IRC Channels and Telegram Chats
+This programm sends messages between IRC Channels and Discord Chats
 
 ## Configuration
 
@@ -10,43 +10,13 @@ Rename it to `config.json` after you are done configuring.
 
 You can read all about the `irc_config` part [here](https://aatxe.github.io/irc/irc/client/data/config/struct.Config.html) (The Field parts is what u are probably most interested in.)
 
-You will need a Telegram API Token. You can read about how to get one [here](https://core.telegram.org/bots#botfather)
+You will need a Discord Bot Token. You can generate one [here](https://discordapp.com/developers/applications/me)
 
 The `mapping` section contains mappings in both directions.
-Use `tg2irc` to send messages from Telegram to IRC and `irc2tg` to send from IRC to Telegram.
+Use `discord2irc` to send messages from Telegram to IRC and `irc2discord` to send from IRC to Telegram.
 
-### Getting the Chat ID for Telegram Chats
-
-This is a bit tricky, since there is no option other than to use the api directly:
-
-1. go to: https://api.telegram.org/bot&lt;token&gt;/getUpdates (replace `<token>` with the bot token you got before)
-2. in the result you will get an array of messages (if there are any new messages, better write a few times in the chat where the bot is):
-
-    ```js
-    {
-        "ok": true,
-        "result": [
-            {
-                "message": {
-                    "chat": {
-                        "all_members_are_administrators": true,
-                        "id": -13371337,
-                        "title": "myAwesomeGroup",
-                        "type": "group"
-                    },
-                    "date": 1483441022,
-                    "from": {
-                        "...": "..."
-                    },
-                    "message_id": 1337,
-                    "text": "foo"
-                },
-                "update_id": 42133742
-            }
-        ]
-    }
-    ```
-3. look for the correct `message->chat->id` value. This will be most likely negative for group chats and positive for normal chats.
+You can get the chat ID in discord by going to `settings->appearance` and selecting the `Developer Mode`  
+You can now just rightclick a channel and copy the id.`
 
 ### filterchars
 Messages which start with any of the characters in the string will be ignored (in both directions). This is nice to prevent sending bot commands around.
--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 extern crate irc;
-extern crate telegram_bot;
 extern crate rustc_serialize;
+extern crate discord;
 
 use std::fs::File;
 use std::io::prelude::*;
@@ -11,26 +11,27 @@ use std::collections::HashMap;
 use std::hash::{Hash, Hasher};
 use std::collections::hash_map::DefaultHasher;
 
-use telegram_bot::*;
+use discord::Discord;
+use discord::model::{Event, ChannelId};
 use irc::client::prelude::*;
 use rustc_serialize::json::decode;
 
 
 #[derive(Clone, RustcDecodable, RustcEncodable, PartialEq, Debug)]
-struct TgConfig {
-    api_token: String,
+struct DiscordConfig {
+    bot_token: String,
 }
 
 #[derive(Clone, RustcDecodable, RustcEncodable, PartialEq, Debug)]
 struct Mapping {
-    tg2irc: HashMap<i64, String>,
-    irc2tg: HashMap<String, i64>,
+    discord2irc: HashMap<u64, String>,
+    irc2discord: HashMap<String, u64>,
 }
 
 #[derive(Clone, RustcDecodable, RustcEncodable, PartialEq, Debug)]
 struct Config {
     irc_config: irc::client::data::Config,
-    tg_config: TgConfig,
+    discord_config: DiscordConfig,
     mapping: Mapping,
     filterchars: String,
 }
@@ -62,74 +63,73 @@ fn main() {
     println!("Starting bridge");
     let config = load("config.json").unwrap();
     let irc_config = config.irc_config;
-    let tg_config = config.tg_config;
-    let tg2irc = config.mapping.tg2irc;
-    let irc2tg = config.mapping.irc2tg;
+    let discord_config = config.discord_config;
+    let discord2irc = config.mapping.discord2irc;
+    let irc2discord = config.mapping.irc2discord;
     let filterchars = config.filterchars;
 
     let irc_server = IrcServer::from_config(irc_config).unwrap();
     irc_server.identify().unwrap();
 
-    let tg_api = Api::from_token(&tg_config.api_token).unwrap();
-    let mut listener = tg_api.listener(ListeningMethod::LongPoll(None));
+    let discord_api = Discord::from_bot_token(&discord_config.bot_token).unwrap();
+    let (mut listener, _) = discord_api.connect().unwrap();
 
     println!("Bridge started.");
 
     let filterchars_ = filterchars.clone();
     let iserver2 = irc_server.clone();
+
     let _ = spawn(move || {
-        let _ = listener.listen(|u| {
-            if let Some(m) = u.message {
-                let name = m.from.username.unwrap_or(m.from.first_name);
-
-                let id = match m.chat {
-                    Chat::Private { id, .. } => id,
-                    Chat::Group { id, .. } => id,
-                    Chat::Channel { id, .. } => id,
-                };
-
-                if let Some(target) = tg2irc.get(&id) {
-                    // for now only MessageType::Text is supported
-                    if let MessageType::Text(msg) = m.msg {
-                        if filterchars_.chars().all(|c| !msg.starts_with(c)) {
-                            iserver2.send_privmsg(target,
-                                              &format!("<\x03{}{}\x03> {}",
-                                                       colorize(&name),
-                                                       name,
-                                                       msg))?;
+        loop {
+            match listener.recv_event() {
+                Ok(Event::MessageCreate(msg)) => {
+                    if msg.author.bot {
+                        continue; // ignore bots
+                    }
+                    if filterchars_.chars().any(|c| msg.content.starts_with(c)) {
+                        continue;
+                    }
+                    if let Some(target) = discord2irc.get(&msg.channel_id.0) {
+                        match iserver2.send_privmsg(target,
+                                                    &format!("<\x03{}{}\x03> {}",
+                                                             colorize(&msg.author.name),
+                                                             msg.author.name,
+                                                             msg.content)) {
+                            Ok(_) => continue,
+                            Err(e) => println!("Error writing to irc: {:?}", e),
                         }
                     }
-                }
-            }
 
-            // If none of the "try!" statements returned an error: It's Ok!
-            Ok(ListeningAction::Continue)
-        });
+                }
+                Ok(_) => continue,
+                Err(e) => println!("Discord recv event error: {:?}", e),
+            }
+        }
     });
 
-    let _ =
-        spawn(move || {
-                for msg in irc_server.iter() {
-                    let msg = msg.unwrap();
-                    if let Command::PRIVMSG(ref target, ref content) = msg.command {
-                        if filterchars.chars().any(|c| content.starts_with(c)) {
-                            continue;
-                        }
-                        if let Some(target) = irc2tg.get(target) {
-                            msg.source_nickname().map(|nick| {
-                                if let Err(e) = tg_api.send_message(*target,
-                                                  format!("*<{}>* {}", nick, content),
-                                                  Some(types::ParseMode::Markdown),
-                                                  None,
-                                                  None,
-                                                  None) {
-                                    println!("{:?}", e);
-                                }
-                            });
-                        }
-                    };
 
-                }
-            })
-            .join();
+    let _ = spawn(move || {
+            for msg in irc_server.iter() {
+                let msg = msg.unwrap();
+                if let Command::PRIVMSG(ref target, ref content) = msg.command {
+                    if filterchars.chars().any(|c| content.starts_with(c)) {
+                        continue;
+                    }
+                    if let Some(target) = irc2discord.get(target) {
+                        msg.source_nickname().map(|nick| {
+                            if let Err(e) = discord_api.send_message(&ChannelId(*target),
+                                                                     &format!("*<{}>* {}",
+                                                                              nick,
+                                                                              content),
+                                                                     "",
+                                                                     false) {
+                                println!("{:?}", e);
+                            }
+                        });
+                    }
+                };
+
+            }
+        })
+        .join();
 }
